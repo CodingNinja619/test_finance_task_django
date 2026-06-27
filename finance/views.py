fdeec7a46b4254475721c029fd352c6b72015941
CodingNinja619 <78737618+CodingNinja619@users.noreply.github.com>
2026-06-27 17:38:48 +0300

    Refactored code using forms.Form

--- finance/views.py
+++ finance/views.py
@@ -6,107 +6,52 @@ from .models import Category, Status, SubCategory, Transaction, Type
 
 from django.http import JsonResponse
 
+from .forms import TransactionFilterForm
+
 # view главной страницы
 def transaction_list(request):
+   
+    form = TransactionFilterForm(request.GET or None)
+
     # Сразу подгружаем необходимые поля
-    transactions = Transaction.objects.select_related(
+    qs = Transaction.objects.select_related(
         "status", "type", "category", "subcategory"
     )
-
-    # Получение полей
-    status_id = request.GET.get("status")
-    type_id = request.GET.get("type")
-    category_id = request.GET.get("category")
-    subcategory_id = request.GET.get("subcategory")
-    min_amount = request.GET.get("min_amount")
-    max_amount = request.GET.get("max_amount")
-    comment = request.GET.get("comment", "").strip()
-    date_from = request.GET.get("date_from")
-    date_to = request.GET.get("date_to")
-
     # Фильтрация
-    if status_id:
-        transactions = transactions.filter(status_id=status_id)
-    
-    if type_id:
-        transactions = transactions.filter(type_id=type_id)
+    if form.is_valid():
+        cd = form.cleaned_data
 
-    if category_id:
-        transactions = transactions.filter(category_id=category_id)
-    
-    if subcategory_id:
-        transactions = transactions.filter(subcategory_id=subcategory_id)
-    
-    if min_amount:
-        try:
-            transactions = transactions.filter(amount__gte=Decimal(min_amount))
-        except InvalidOperation:
-            pass
-    
-    if max_amount:
-        try:
-            transactions = transactions.filter(amount__lte=Decimal())
-        except InvalidOperation:
-            pass
-    
-    if comment:
-        transactions = transactions.filter(comment__icontains=comment)
-    
-    if date_from:
-        transactions = transactions.filter(created_at__gte=date_from)
-    
-    if date_to:
-        transactions = transactions.filter(created_at__lte=date_to)
+        if cd["date_from"]:
+            qs = qs.filter(created_at__gte=cd["date_from"])
+        if cd["date_to"]:
+            qs = qs.filter(created_at__lte=cd["date_to"])
 
-    # if status_id:
-    #     transactions = transactions.filter(status_id=status_id)
-    # if type_id:
-    #     transactions = transactions.filter(type_id=type_id)
-    # if category_id:
-    #     transactions = transactions.filter(category_id=category_id)
-    # if subcategory_id:
-    #     transactions = transactions.filter(subcategory_id=subcategory_id)
-    # if min_amount:
-    #     try:
-    #         transactions = transactions.filter(amount__gte=Decimal(min_amount))
-    #     except InvalidOperation:
-    #         pass
-    # if max_amount:
-    #     try:
-    #         transactions = transactions.filter(amount__lte=Decimal(max_amount))
-    #     except InvalidOperation:
-    #         pass
-    # if comment:
-    #     transactions = transactions.filter(comment__icontains=comment)
-    # if date_from:
-    #     transactions = transactions.filter(created_at__gte=date_from)
-    # if date_to:
-    #     transactions = transactions.filter(created_at__lte=date_to)
+        if cd["status"]:
+            qs = qs.filter(status=cd["status"])
 
-    filters = {
-        "date_from": date_from or "",
-        "date_to": date_to or "",
-        "status": status_id or "",
-        "type": type_id or "",
-        "category": category_id or "",
-        "subcategory": subcategory_id or "",
-        "min_amount": min_amount or "",
-        "max_amount": max_amount or "",
-        "comment": comment,
-    }
-    # if type_id:
-    #     categories = Category.objects.filter(type_id=type_id)
-    # if category_id:
-    #     subcategories = SubCategory.objects.filter(category_id=category_id)
+        if cd["type"]:
+            qs = qs.filter(type=cd["type"])
+
+        if cd["category"]:
+            qs = qs.filter(category=cd["category"])
+
+        if cd["subcategory"]:
+            qs = qs.filter(subcategory=cd["subcategory"])
+
+        if cd["min_amount"]:
+            qs = qs.filter(amount__gte=cd["min_amount"])
+
+        if cd["max_amount"]:
+            qs = qs.filter(amount__lte=cd["max_amount"])
+
+        if cd["comment"]:
+            qs = qs.filter(comment__icontains=cd["comment"])
+
 
     context = {
-        "transactions": transactions,
-        "statuses": Status.objects.all(),
-        "types": Type.objects.all(),
-        "categories": Category.objects.filter(),
-        "subcategories": SubCategory.objects.all(),
-        "filters": filters,
-        "has_filters": any(filters.values()),
+        "transactions": qs,
+        "form": form,
+
     }
     return render(request, "finance/transaction_list.html", context)
 
